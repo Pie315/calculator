fb3da9a13bb03a5742c5fa95b18ac854cf02c7cc
Pie315 <devonkaelberer@gmail.com>
2023-01-19 22:52:37 -0500

    listeners and screen functions

--- javascript.js
+++ javascript.js
@@ -5,17 +5,40 @@ let b = 0;
 let operation = "";
 
 // main logic
-
 function operate(a, b, operation) {
     switch(operation) {
-        case "add": 
+        case "+": 
             return (a + b);
-        case "multiply":
+        case "x":
             return (a * b);
-        case "minus":
+        case "-":
             return (a - b);
-        case "divide":
+        case "/":
             return (a / b);
     }
 }
 
+// screens
+function changeTop(cont) {
+    topScreen.textContent = cont;
+}
+function changeBot(cont) {
+    botScreen.textContent = cont;
+}
+
+
+// event listeners
+function press(element) {
+    console.log(element.id);
+    changeTop(element.id);
+    changeBot(element.id);
+}
+
+let buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => press(button));
+});
+
+let topScreen = document.querySelector(".screenTop");
+let botScreen = document.querySelector(".screenBot");
