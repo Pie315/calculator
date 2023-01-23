4650f6a3c4cd121971ff82417baf1e78f8a1d527
Pie315 <devonkaelberer@gmail.com>
2023-01-23 15:04:44 -0500

    operator logic

--- javascript.js
+++ javascript.js
@@ -3,7 +3,12 @@ console.log("code works");
 let a = 0;
 let b = 0;
 let operation = "";
-let memory = "";
+let memoryBot = "";
+let last = "";
+let decimal = false;
+let topScreenContent = "";
+let botScreenContent = "";
+
 
 // main logic
 function operate(a, b, operation) {
@@ -19,32 +24,93 @@ function operate(a, b, operation) {
     }
 }
 
+function calculate() {
+    changeTop(memoryBot);
+
+
+
+
+
+}
+
+
+
 // screens
 function changeTop(cont) {
+    topScreenContent = cont;
     topScreen.textContent = cont;
 }
 function changeBot(cont) {
+    botScreenContent = cont;
     botScreen.textContent = cont;
 }
 
+function clearMem() {
+    changeTop("");
+    changeBot("");
+    memoryBot = "";
+    decimal = false;
+    operator = undefined;
+}
+
+function deleteMem() {
+    let length = memoryBot.length;
+
+    console.log(memoryBot[length]);
+    switch (memoryBot[length]) {
+        case "+":
+            operator = undefined;
+            break;
+        case "-":
+            operator = undefined;
+            break;
+        case "/":
+            operator = undefined;
+            break;
+        case "x":
+            operator = undefined;
+            break;
+        case ".":
+            decimal=false;
+            break;
+    }
+
+
+    memoryBot = memoryBot.slice(0, length - 1);
+    changeBot(memoryBot);
+}
 
 // event listeners
 function pressOperation(element) { // elements
-    if (memory == "") {
+    if (memoryBot == "") {
         return;
-    } 
-    
-    
+    } else if (element.id == "clear") {
+        clearMem();
+        return;
+    } else if (element.id == "delete") {
+        deleteMem();
+        return;
+    } else if (element.id == "=") {
+        calculate();
+        return;
+    } else if ( element.id == ".") {
+        if (decimal == true) {
+            return;
+        }
+        decimal = true;
+    }
+
+    memoryBot += element.id;
+    changeBot(memoryBot);
 
 }
 
 function pressNumber(number) { // numbers
+    memoryBot += number.id;
 
-    let length = memory.length;
-    memory += number.id;
+    changeBot(memoryBot);
 
-    changeBot(memory);
-    console.log(memory);
+
 }
 
 
